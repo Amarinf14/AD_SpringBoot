ea5327e34f34e3cec6bacbd293f4540acc0ec657
Alberto Marín <macramacramacra@gmail.com>
2026-02-05 12:45:38 +0100

    Creación del proyecto

---
+++ Marin-guiada-Spring/src/main/java/com/spring/marin/Marin_guiada_Spring/MarinGuiadaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.marin.Marin_guiada_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinGuiadaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarinGuiadaSpringApplication.class, args);
+	}
+
+}
