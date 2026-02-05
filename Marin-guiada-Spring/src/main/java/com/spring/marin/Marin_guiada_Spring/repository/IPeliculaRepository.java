53e7b2a00e2a5228bb2a5984701d90c776c1d576
Alberto Marín <macramacramacra@gmail.com>
2026-02-05 13:17:27 +0100

    Creación de la interfaz del repositorio

---
+++ Marin-guiada-Spring/src/main/java/com/spring/marin/Marin_guiada_Spring/repository/IPeliculaRepository.java
@@ -0,0 +1,10 @@
+package com.spring.marin.Marin_guiada_Spring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IPeliculaRepository extends CrudRepository {
+    /*
+    CrudRepository es una interfaz provista por Spring Data que ofrece métodos básicos para realizar operaciones CRUD en la base de datos.
+    Para funciones más avanzadas, existe JpaRepository, que extiende CrudRepository
+     */
+}
