b5734ba5486e06500600c4100481164be4b5214e
Alberto Marín <macramacramacra@gmail.com>
2026-02-05 13:49:18 +0100

    Implementación de métodos específicos y documentación

--- Marin-guiada-Spring/src/main/java/com/spring/marin/Marin_guiada_Spring/service/IClienteService.java
+++ Marin-guiada-Spring/src/main/java/com/spring/marin/Marin_guiada_Spring/service/IClienteService.java
@@ -1,4 +1,30 @@
 package com.spring.marin.Marin_guiada_Spring.service;
 
+import com.spring.marin.Marin_guiada_Spring.model.Peliculas;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Interfaz que define la lógica de negocio de la aplicación.
+ * Actúa como capa intermedia entre el controlador y el repositorio, gestionando
+ * las operaciones necesarias antes de persistir o recuperar datos
+ *
+ * @author Alberto Marín Fernández
+ */
 public interface IClienteService {
+
+    Iterable<Peliculas> obtenerTodas();
+
+    Peliculas guardarPelicula(Peliculas pelicula);
+
+    Optional<Peliculas> obtenerPorId(Long id);
+
+    void eliminarPelicula(Long id);
+
+    List<Peliculas> buscarPorGenero(String genero);
+
+    List<Peliculas> buscarPorDirector(String director);
+
+    List<Peliculas> buscarPorTitulo(String keyword);
 }
