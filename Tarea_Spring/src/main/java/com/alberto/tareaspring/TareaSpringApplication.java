08865d93dfab72e19d838b2f8c6edf7827e7381c
Alberto Marín <macramacramacra@gmail.com>
2026-02-09 10:15:53 +0100

    Creación de nuevo proyecto

---
+++ Tarea_Spring/src/main/java/com/alberto/tareaspring/TareaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.tareaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaSpringApplication.class, args);
+	}
+
+}
---
+++ Tarea_Spring/src/test/java/com/alberto/tareaspring/TareaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.tareaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
